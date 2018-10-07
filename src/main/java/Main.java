e56e0c2a117d8914a31eeb34cfbcafa2ede9ca99
Robert Sadowksi <robertsadowski005@gmail.com>
2018-10-07 09:35:50 +0200

    Nowa linia

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Zamotanie");
+        System.out.println("trzecia");
         System.out.println("coś nowego");
     }
 }
